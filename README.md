8e88cd64f06ed177d09423a83b3f6db389c8511c
Sybren <92291687+sybrenkeizer@users.noreply.github.com>
2022-03-28 23:31:09 +0530

    Finish working game

--- README.md
+++ README.md
@@ -2,18 +2,22 @@
 
 The Odin Project - Foundations
 
-Project #3: Landing page
+Project #4: Rock, Paper and Scissors
 
 Source: https://sybrenkeizer.github.io/top-rock-paper-scissors/
 
-Languages: JS
+Language: JS
 
 Date start: 28/03/2022
 
-Date finish: .../03/2022
+Date finish: 28/03/2022
 
-Hours:
+Hours: 9 hours
 
 Learned:
 
+  - Interaction between multiple functions
+
 Improve:
+
+  - Implementatiojn of ternary operators
--- script.js
+++ script.js
@@ -1,23 +1,77 @@
+// Rock, paper and scissors game
+
+let message = ''
+let playerScore = 0;
+let computerScore = 0;
+
 // Ask to play the game
 let agreeToPlay = confirm(`Would you like to play a game of "Rock, Paper and Scissors"?`);
 if (agreeToPlay) {
-  // Request choice tool
-  let playerChoice = prompt(`Please make your choice: "Rock", "Paper" or "Scissors"`)
-  if (playerChoice) {
-    // Make sure input is correct
-    if (playerChoice === `Rock` || playerChoice === `Paper` || playerChoice === `Scissors`) {
-      // Start with game
-      document.write(`start game`)
 
+  // Request choice tool
+  function playRound() {
+    let playerInput = prompt(`Please make your choice: "Rock", "Paper" or "Scissors"`)
+    if (playerInput) {
+
+      // Make sure input is correct
+      let playerChoice = playerInput.trim().toLowerCase()
+      if (playerChoice === `rock` || playerChoice === `paper` || playerChoice === `scissors`) {
+
+        // Start with game
+        function computerPlay() {
+          let options = [`rock`, `paper`, `scissors`]
+          let randomize = Math.floor(Math.random() * options.length)
+          return options[randomize]
+        }
+        let computerChoice = computerPlay()
+        if (playerChoice === computerChoice) {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} -It's a tie!`;
+        } else if (playerChoice === `rock` && computerChoice === `paper`) {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} - You lose! Paper beats rock.`;
+          computerScore++;
+        } else if (playerChoice === `rock` && computerChoice === `scissors`) {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} - You win! Rock beats scissors.`;
+          playerScore++;
+        } else if (playerChoice === `paper` && computerChoice === `scissors`) {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} - You lose! scissors beats paper.`;
+          computerScore++;
+        } else if (playerChoice === `paper` && computerChoice === `rock`) {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} - You win! paper beats rock.`;
+          playerScore++;
+        } else if (playerChoice === `scissors` && computerChoice === `rock`) {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} - You lose! Rock beats scissors.`;
+          computerScore++;
+        } else if (playerChoice === `scissors` && computerChoice === `paper`) {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} - You win! scissors beats paper.`;
+          playerScore++;
+        } else {
+          message = `Player: ${playerChoice}, Computer: ${computerChoice} - ERROR: WHAT IS THIS`;
+        }
+      } else {
+        alert(`You did not entered the correct tool, please try again or abort.`)
+        // location.reload()
+      }
     } else {
-      alert(`You did not entered the correct tool, please try again or abort.`)
-      location.reload()
+      // Cancel, esc or choose to not fill in field
+      alert(`No problem, come back when you make up your mind`)
     }
-  } else {
-    // Cancel, esc or choose to not fill in field
-    alert(`No problem, come back when you make up your mind`)
   }
 } else {
   // Press cancel or esc
   alert(`Oke, maybe next time`)
+}
+
+for (let i = 0; i < 5; i++) {
+  playRound()
+  console.log(message);
+  console.log(`playerscore: ${playerScore}`);
+  console.log(`computerscore: ${computerScore}`);
+}
+
+if (playerScore > computerScore) {
+  console.log(`End of game, you have won with a score of ${playerScore}  against a computer score of ${computerScore}`);
+} else if (computerScore > playerScore) {
+  console.log(`End of game, you have lost with a score of ${playerScore}  against a computer score of ${computerScore}`);
+} else if (computerScore === playerScore) {
+  console.log(`End of game, it's a tie with you having a score of ${playerScore} against a computer score of ${computerScore}`);
 }
